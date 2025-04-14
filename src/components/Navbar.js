6e7cffa28f119f70de173b1b42d0a29100ee7f1c
21-abhishekyadav <21abhishek.ay@gmail.com>
2025-04-14 13:31:59 +0530

    reset button in nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,75 @@
 import React from 'react'
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import {useState } from 'react'
 import logo from './images/logo.png'
-export default function Navbar() {
-  
 
-  
-  
+
+
+
+const reset = () => {
+  const categoryData = JSON.parse(localStorage.getItem("categoryEngagement")) || {};
+
+  // Reset all values to 0
+  const resetData = {};
+  for (const category in categoryData) {
+    resetData[category] = 1;
+  }
+
+  // If no data existed before, initialize defaults
+  if (Object.keys(resetData).length === 0) {
+    const defaultCategories = [
+      "Technology",
+      "Health & Wellness",
+      "Education & Learning",
+      "Lifestyle & Productivity",
+      "Entertainment & Pop Culture",
+      "General"
+    ];
+    defaultCategories.forEach(category => {
+      resetData[category] = 1;
+    });
+  }
+
+  localStorage.setItem("categoryEngagement", JSON.stringify(resetData));
+};
+
+
+export default function Navbar() {
+  const [showResetButton, setShowResetButton] = useState(false);
+
+  const show = () => {
+    if(showResetButton){
+      setShowResetButton(false);
+    }
+    else{
+    setShowResetButton(true);
+    }
+
+  };
 
   return (
-    <div >
-      <nav className="navbar " style={{
-        zIndex:'100',
-        width:'100%',
-        height:'10vh'
-        }}>
-          <div  style={{
-        backgroundColor: '#DBD8E3',
-        height :'10vh',
-        width:'100%',  
-        
-        position:'fixed'                 
-      }}>
-          <div>
-          <Link className="navbar-brand" to="/">
-            <img src={logo} alt="Logo" style={{height:'10vh' }} />
-          </Link> 
-          
-                        
+    <div>
+  <nav className="navbar" style={{
+    zIndex: '100',
+    width: '100%',
+    height: '10vh'
+  }}>
+    <div style={{
+      backgroundColor: '#DBD8E3',
+      height: '10vh',
+      width: '100%',
+      position: 'fixed'
+    }}>
+      <div className="d-flex justify-content-between align-items-center w-100 h-100 px-3">
+        <div onClick={show}>
+          <img src={logo} alt="Logo" style={{ height: '10vh' }} />
         </div>
-
-          </div>
-        
-      </nav>
+        {showResetButton && (<button className="btn btn-outline-danger" onClick={reset}>
+          <i className="bi bi-arrow-counterclockwise"></i>
+        </button>)}
+      </div>
     </div>
+  </nav>
+</div>
   )
 }
 
--- src/context/PostState.js
+++ src/context/PostState.js
@@ -7,30 +7,28 @@ export default function NoteState(props) {
     
  
     const [posts, setPosts] = useState([]);
-    const defaultCategories = [
-        "Technology",
-        "Health & Wellness",
-        "Education & Learning",
-        "Lifestyle & Productivity",
-        "Entertainment & Pop Culture",
-        "General"
-      ];
+    const defaultCategories = {
+        "Technology": 1,
+        "Health & Wellness": 1,
+        "Education & Learning": 1,
+        "Lifestyle & Productivity": 1,
+        "Entertainment & Pop Culture": 1,
+        "General": 1
+      };
     
 
     //something  
    
     const getpost = async (page = 1, reset = false) => {
         try {
-            const categoryData = JSON.parse(localStorage.getItem("categoryEngagement")) || {};
-            const categoriesToUse = Object.keys(categoryData).length ? 
-                Object.entries(categoryData)
-                .sort(([, a], [, b]) => b - a)
-                .map(([category]) => category)
-            : defaultCategories;
-            let totalEngagement = Object.values(categoryData).reduce((sum, count) => sum + count, 0) || 1;
+            const categoryData = JSON.parse(localStorage.getItem("categoryEngagement")) || defaultCategories;
+            const sortedCategories = Object.entries(categoryData)
+            .sort(([, a], [, b]) => b - a)
+            .map(([category]) => category);
+            const totalEngagement = Object.values(categoryData).reduce((sum, count) => sum + count, 0) ||1;
             let allPosts = [];
 
-            for (const category of categoriesToUse) {
+            for (const category of sortedCategories) {
                 let categoryLimit = categoryData[category]
                   ? Math.round((categoryData[category] / totalEngagement) * 10)
                   : 1;
